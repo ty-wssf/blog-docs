f20c7747d56250ea10668036b794873c942976e0
wyl <791128058@qq.com>
2021-08-17 22:20:37 +0800

    部署调整

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
     title: '吴益龙的博客',
     description: '吴益龙的博客',
     // 部署到GitHub上设置基础路径，部署到vercel上不设置基础路径
-    base: 'blog-docs',
+    //base: 'blog-docs',
     // 主题配置
     themeConfig: {
         // 最后更新时间
